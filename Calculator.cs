adfb24ca9b89b50b49822b526cad53250b2cca0f
krisdixon <51926707+krisdixon@users.noreply.github.com>
2019-09-05 10:20:42 -0500

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using CalculatorWithTests;
+using System.Linq;
+
+
+
+namespace CalculatorWithTests
+{
+    public class Calculator
+    {
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+        public int Subtract(int sub1, int sub2)
+        {
+            return sub1 - sub2;
+        }
+        public int Multiply(int multiply1, int multiply2)
+        {
+            return multiply1 * multiply2;
+        }
+        public decimal Divide(decimal dividend, decimal divisor)
+        {
+            return dividend / divisor;
+        }
+        public long Factorial(long factNumber)
+        {
+            long result = 1;
+            for (int i = 1; i <= factNumber; i++)
+            {
+                result = result * i;
+            }
+            return result;
+                
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
